72d750ce3e8b02ffd1fcfe803e2b00975d3bf9f3
DaniPoka <danisschulz@gmail.com>
2023-10-15 16:33:50 -0300

    asd

---
+++ algo.js
@@ -0,0 +1,13 @@
+let num1 = 3;
+let num2 = 8;
+let num3 = 4;
+
+let mayor = num1;
+
+if (num2 > mayor) {
+    mayor = num2;
+} if (num3 > mayor) {
+    mayor = num3;
+}
+
+console.log('el numero mas grande es ' + mayor);
